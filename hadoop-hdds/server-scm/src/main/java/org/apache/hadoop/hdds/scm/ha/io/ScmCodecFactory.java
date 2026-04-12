51532379f53df0a0ed4b850e7004889becdbf78e
Tsz-Wo Nicholas Sze <szetszwo@apache.org>
2026-04-12 10:38:14 -0700

    HDDS-15022. Make ScmCodecFactory.classes map unmodifiable.

--- hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/io/ScmCodecFactory.java
+++ hadoop-hdds/server-scm/src/main/java/org/apache/hadoop/hdds/scm/ha/io/ScmCodecFactory.java
@@ -19,6 +19,7 @@ package org.apache.hadoop.hdds.scm.ha.io;
 
 import com.google.protobuf.Message;
 import com.google.protobuf.ProtocolMessageEnum;
+import java.lang.reflect.Method;
 import java.math.BigInteger;
 import java.security.cert.X509Certificate;
 import java.util.Collection;
@@ -39,55 +40,32 @@ import org.apache.hadoop.hdds.protocol.proto.HddsProtos.Pipeline;
 import org.apache.hadoop.hdds.protocol.proto.HddsProtos.PipelineID;
 import org.apache.hadoop.hdds.protocol.proto.HddsProtos.PipelineState;
 import org.apache.hadoop.hdds.protocol.proto.StorageContainerDatanodeProtocolProtos.DeletedBlocksTransaction;
+import org.apache.hadoop.hdds.scm.ha.SCMHandler;
+import org.apache.hadoop.hdds.scm.metadata.Replicate;
 import org.apache.hadoop.hdds.security.symmetric.ManagedSecretKey;
 import org.apache.ratis.thirdparty.com.google.protobuf.ByteString;
 import org.apache.ratis.thirdparty.com.google.protobuf.InvalidProtocolBufferException;
+import org.apache.ratis.util.Preconditions;
+import org.reflections.Reflections;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Maps types to the corresponding {@link ScmCodec} implementation.
  */
 public final class ScmCodecFactory {
+  static final Logger LOG = LoggerFactory.getLogger(ScmCodecFactory.class);
 
-  private final Map<Class<?>, ScmCodec<?>> codecs = new HashMap<>();
-  private final Map<String, Class<?>> classes = new ConcurrentHashMap<>();
+  private final Map<Class<?>, ScmCodec<?>> codecs;
+  private final Map<String, Class<?>> classes;
   private final ClassResolver resolver;
   private static final ScmCodecFactory INSTANCE = new ScmCodecFactory();
 
   private ScmCodecFactory() {
-    putProto(ContainerID.getDefaultInstance());
-    putProto(PipelineID.getDefaultInstance());
-    putProto(Pipeline.getDefaultInstance());
-    putProto(ContainerInfoProto.getDefaultInstance());
-    putProto(DeletedBlocksTransaction.getDefaultInstance());
-    putProto(DeletedBlocksTransactionSummary.getDefaultInstance());
-
-    codecs.put(Integer.class, new ScmIntegerCodec());
-    codecs.put(Long.class, new ScmLongCodec());
-    codecs.put(String.class, new ScmStringCodec());
-    codecs.put(Boolean.class, new ScmBooleanCodec());
-    codecs.put(BigInteger.class, new ScmBigIntegerCodec());
-    codecs.put(X509Certificate.class, new ScmX509CertificateCodec());
-    codecs.put(com.google.protobuf.ByteString.class, new ScmNonShadedByteStringCodec());
-    codecs.put(ByteString.class, new ScmByteStringCodec());
-    codecs.put(ManagedSecretKey.class, new ScmManagedSecretKeyCodec());
-
-    putEnum(LifeCycleEvent.class, LifeCycleEvent::forNumber);
-    putEnum(PipelineState.class, PipelineState::forNumber);
-    putEnum(NodeType.class, NodeType::forNumber);
-
-    // Must be the last one
-    resolver = new ClassResolver(codecs.keySet());
-    codecs.put(List.class, new ScmListCodec(resolver));
-  }
-
-  private <T extends Message> void putProto(T proto) {
-    final Class<? extends Message> clazz = proto.getClass();
-    codecs.put(clazz, new ScmNonShadedGeneratedMessageCodec<>(clazz.getSimpleName(), proto.getParserForType()));
-  }
-
-  private <T extends Enum<T> & ProtocolMessageEnum> void putEnum(
-      Class<T> enumClass, IntFunction<T> forNumber) {
-    codecs.put(enumClass, new ScmEnumCodec<>(enumClass, forNumber));
+    final Builder b = new Builder();
+    this.codecs = Collections.unmodifiableMap(b.codecs);
+    this.classes = Collections.unmodifiableMap(b.classes);
+    this.resolver = b.resolver;
   }
 
   public static ScmCodecFactory getInstance() {
@@ -113,18 +91,12 @@ public final class ScmCodecFactory {
 
   public Class<?> getClass(String className) throws InvalidProtocolBufferException {
     final Class<?> found = classes.get(className);
-    if (found != null) {
-      return found;
+    if (found == null) {
+      final IllegalStateException e = new IllegalStateException("Class not found for " + className);
+      LOG.error("Failed", e);
+      throw e;
     }
-
-    final Class<?> clazz;
-    try {
-      clazz = Class.forName(className);
-    } catch (ClassNotFoundException e) {
-      throw new InvalidProtocolBufferException("Class not found for " + className, e);
-    }
-    classes.put(className, clazz);
-    return clazz;
+    return found;
   }
 
   /** Resolve the codec class from a given class. */
@@ -173,6 +145,108 @@ public final class ScmCodecFactory {
       }
 
       throw new InvalidProtocolBufferException("Failed to resolve " + className);
+    }
+  }
+
+  static final class Builder {
+    private final Reflections apache = new Reflections("org.apache");
+    private final Reflections[] reflections = {
+        apache,
+        new Reflections("java"),
+        new Reflections("com.google"),
+    };
+
+    private final Map<Class<?>, ScmCodec<?>> codecs = new HashMap<>();
+    private final Map<String, Class<?>> classes = new TreeMap<>();
+    private final ClassResolver resolver;
+
+    private Builder() {
+      putProto(ContainerID.getDefaultInstance());
+      putProto(PipelineID.getDefaultInstance());
+      putProto(Pipeline.getDefaultInstance());
+      putProto(ContainerInfoProto.getDefaultInstance());
+      putProto(DeletedBlocksTransaction.getDefaultInstance());
+      putProto(DeletedBlocksTransactionSummary.getDefaultInstance());
+
+      codecs.put(Integer.class, new ScmIntegerCodec());
+      codecs.put(Long.class, new ScmLongCodec());
+      codecs.put(String.class, new ScmStringCodec());
+      codecs.put(Boolean.class, new ScmBooleanCodec());
+      codecs.put(BigInteger.class, new ScmBigIntegerCodec());
+      codecs.put(X509Certificate.class, new ScmX509CertificateCodec());
+      codecs.put(com.google.protobuf.ByteString.class, new ScmNonShadedByteStringCodec());
+      codecs.put(ByteString.class, new ScmByteStringCodec());
+      codecs.put(ManagedSecretKey.class, new ScmManagedSecretKeyCodec());
+
+      putEnum(LifeCycleEvent.class, LifeCycleEvent::forNumber);
+      putEnum(PipelineState.class, PipelineState::forNumber);
+      putEnum(NodeType.class, NodeType::forNumber);
+
+      // Must be the last one
+      resolver = new ClassResolver(codecs.keySet());
+      codecs.put(List.class, new ScmListCodec(resolver));
+
+      for (Class<?> subclass : apache.getSubTypesOf(SCMHandler.class)) {
+        if (!subclass.isInterface()) {
+          continue;
+        }
+        putReplicateClasses(subclass);
+      }
+
+      classes.values().forEach(value -> LOG.debug("{}", value));
+    }
+
+    private <T extends Message> void putProto(T proto) {
+      final Class<? extends Message> clazz = proto.getClass();
+      putClasses(clazz);
+      codecs.put(clazz, new ScmNonShadedGeneratedMessageCodec<>(clazz.getSimpleName(), proto.getParserForType()));
+    }
+
+    private <T extends Enum<T> & ProtocolMessageEnum> void putEnum(
+        Class<T> enumClass, IntFunction<T> forNumber) {
+      putClasses(enumClass);
+      codecs.put(enumClass, new ScmEnumCodec<>(enumClass, forNumber));
+    }
+
+    private void putReplicateClasses(Class<?> api) {
+      LOG.info("api: {}", api);
+      final Map<String, Method> methods = new HashMap<>();
+      for (Method m : api.getDeclaredMethods()) {
+        if (m.getAnnotation(Replicate.class) == null) {
+          continue;
+        }
+        LOG.info("  method: {}", m);
+
+        // assert that no two methods have the same name and the same parameter count.
+        final Method previous = methods.put(m.getName() + m.getParameterTypes().length, m);
+        if (previous != null) {
+          throw new IllegalStateException("Duplicate method found in " + api + ":\n  " + previous + "\n  " + m);
+        }
+
+        putClasses(m.getReturnType());
+        for (Class<?> clazz : m.getParameterTypes()) {
+          putClasses(clazz);
+        }
+      }
+    }
+
+    private void putClasses(Class<?> clazz) {
+      putClass(clazz);
+      for (Reflections r : reflections) {
+        for (Class<?> sub : r.getSubTypesOf(clazz)) {
+          putClass(sub);
+        }
+      }
+    }
+
+    private void putClass(Class<?> clazz) {
+      if (clazz.isInterface()) {
+        return;
+      }
+      final Class<?> previous = classes.put(clazz.getName(), clazz);
+      if (previous != null) {
+        Preconditions.assertSame(previous, clazz, "class");
+      }
     }
   }
 }
